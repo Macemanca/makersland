385993a1e2149174849e6527667ef7f36933fa63
kitchi <kitchi@Kitchi-Kubuntu>
2017-04-27 13:50:04 -0400

    added TerrainTypes enum

--- makersland/core/src/ca/maceman/makersland/world/terrain/parts/TerrainTriangle.java
+++ makersland/core/src/ca/maceman/makersland/world/terrain/parts/TerrainTriangle.java
@@ -2,7 +2,6 @@ package ca.maceman.makersland.world.terrain.parts;
 
 import ca.maceman.makersland.world.utils.terrain.TerrainUtils;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder.VertexInfo;
 import com.badlogic.gdx.math.Vector3;
 
@@ -15,11 +14,7 @@ public class TerrainTriangle {
 	public TerrainVector vAT;
 	public TerrainVector vAB;
 	public Vector3 normal;
-	public Color colour;
-	public int type;
-	/*
-	 * Types (short sides) : Bottom-Left = 1 BR = 2 TL = 3 TR = 4
-	 */
+	public TerrainType terrainType = TerrainType.BEACH;
 
 	/**
 	 * 
@@ -39,30 +34,20 @@ public class TerrainTriangle {
 
 		normal = TerrainUtils.calcNormal(vRA.toVector3(), vAB.toVector3(), vAT.toVector3());
 
-		colour = new Color(.9f, .8f, .7f, 1f);
-
 		float maxHeight = parentTile.getParentChunk().getParentTerrain().getMaxHeight();
 
 		System.out.println(maxHeight);
-		float step6 = maxHeight * 0.75f;
-		float step5 = maxHeight * 0.65f;
-		float step4 = maxHeight * 0.45f;
-		float step3 = maxHeight * 0.35f;
-		float step2 = maxHeight * 0.20f;
-		float step1 = maxHeight * 0.15f;
- 
-		if (vRA.z >= step6 && vAT.z >= step6 && vAB.z >= step6) {
-			colour = new Color(1f, 1f, 1f, 1f);
-		} else if (vRA.z >= step5 && vAT.z >= step5 && vAB.z >= step5) {
-			colour = new Color(.7f, .7f, .7f, 1f);
-		} else if (vRA.z >= step4 && vAT.z >= step4 && vAB.z >= step4) {
-			colour = new Color(.5f, .6f, .3f, 1f);
-		} else if (vRA.z >= step3 && vAT.z >= step3 && vAB.z >= step3) {
-			colour = new Color(.2f, .4f, .2f, 1f);
-		} else if (vRA.z >= step2 && vAT.z >= step2 && vAB.z >= step2) {
-			colour = new Color(.3f, .6f, .3f, 1f);
-		} else if (vRA.z >= step1 && vAT.z >= step1 && vAB.z >= step1) {
-			colour = new Color(.5f, .7f, .5f, 1f);
+
+		if (vRA.z >= maxHeight * 0.75f && vAT.z >= maxHeight * 0.75f && vAB.z >= maxHeight * 0.75f) {
+			terrainType = TerrainType.SNOWY_PEAKS;
+		} else if (vRA.z >= maxHeight * 0.45f && vAT.z >= maxHeight * 0.45f && vAB.z >= maxHeight * 0.45f) {
+			terrainType = TerrainType.MOUNTAIN;
+		} else if (vRA.z >= maxHeight * 0.25f && vAT.z >= maxHeight * 0.25f && vAB.z >= maxHeight * 0.25f) {
+			terrainType = TerrainType.BOREAL;
+		} else if (vRA.z >= maxHeight * 0.15f && vAT.z >= maxHeight * 0.15f && vAB.z >= maxHeight * 0.15f) {
+			terrainType = TerrainType.TEMPERATE;
+		} else if (vRA.z >= maxHeight * 0.05f && vAT.z >= maxHeight * 0.05f && vAB.z >= maxHeight * 0.05f) {
+			terrainType = TerrainType.GRASSLAND;
 		}
 
 		vi = new VertexInfo();
@@ -72,7 +57,7 @@ public class TerrainTriangle {
 	public VertexInfo getRIVertexInfo() {
 
 		vi = new VertexInfo();
-		vi.setPos(vRA.toVector3()).setNor(normal).setCol(colour).setUV(vRA.u, vRA.v);
+		vi.setPos(vRA.toVector3()).setNor(normal).setCol(terrainType.colour).setUV(vRA.u, vRA.v);
 
 		return vi;
 	}
@@ -80,7 +65,7 @@ public class TerrainTriangle {
 	public VertexInfo getATVertexInfo() {
 
 		vi = new VertexInfo();
-		vi.setPos(vAT.toVector3()).setNor(normal).setCol(colour).setUV(vAT.u, vAT.v);
+		vi.setPos(vAT.toVector3()).setNor(normal).setCol(terrainType.colour).setUV(vAT.u, vAT.v);
 
 		return vi;
 	}
@@ -89,7 +74,7 @@ public class TerrainTriangle {
 
 		vi = new VertexInfo();
 
-		vi.setPos(vAB.toVector3()).setNor(normal).setCol(colour).setUV(vAB.u, vAB.v);
+		vi.setPos(vAB.toVector3()).setNor(normal).setCol(terrainType.colour).setUV(vAB.u, vAB.v);
 
 		return vi;
 	}
---
+++ makersland/core/src/ca/maceman/makersland/world/terrain/parts/TerrainType.java
@@ -0,0 +1,20 @@
+package ca.maceman.makersland.world.terrain.parts;
+
+import com.badlogic.gdx.graphics.Color;
+
+public enum TerrainType {
+		BEACH("Beach",new Color(.9f, .8f, .7f, 1f)),
+		GRASSLAND("Grass Lands",new Color(.3f, .6f, .3f, 1f)),
+		TEMPERATE("Temperate Forest",new Color(.2f, .4f, .2f, 1f)),
+		BOREAL("Boreal Forest",new Color(.5f, .6f, .3f, 1f)),
+		MOUNTAIN("Mountains",new Color(.7f, .7f, .7f, 1f)),
+		SNOWY_PEAKS("Snowy Peaks",new Color(1f, 1f, 1f, 1f));
+		
+	public String name;
+	public Color colour;
+	
+	private TerrainType(String name, Color colour) {
+		this.name = name;
+		this.colour = colour;
+	}
+}
